9fe4aac27e8271a99bc1f31cbae6521420d0fd7f
Siddhant Tandon <76514713+siddhantt24@users.noreply.github.com>
2024-07-26 01:29:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class EvenOdd
+{
+    static void Main(string[] args)
+    {
+        int n;
+
+        Console.WriteLine("Enter a number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("{0} is even", n);
+        }
+        else
+        {
+            Console.WriteLine("{0} is odd", n);
+        }
+        Console.ReadKey();
+    }
+}
